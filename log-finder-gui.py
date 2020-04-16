a4ad9afe41b8991659272bef6ec5bf33b1bd719c
42810647 <alec@pop-os.localdomain>
2020-04-15 18:28:05 -0700

    add output and menu features

--- log-finder-gui.py
+++ log-finder-gui.py
@@ -1,18 +1,38 @@
 import PySimpleGUI as sg
 import shelve
 import logfinder
+from pathlib import Path
+
+'''shelfFile = shelve.open(str(Path('shelf/prodpath')))
+prodPath = Path('Prod/')
+shelfFile['prodPath'] = str(prodPath)
+shelfFile.close()
+
+shelfFile = shelve.open(str(Path('shelf/prodpath')))
+print(list(shelfFile.values()))
+shelfFile.close()
+
+quit()'''
 
 # color theme, use sg.theme_previewer() to see all theme options
 sg.theme('BrightColors')
 
-layout = [  [sg.Text("Work Order #:")],
+menu_def = [ ['File', ['Exit']],
+            ['Edit', ['Search Paths']] ]
+
+layout = [  [sg.Menu(menu_def)],
+            [sg.Text("Work Order #:")],
             [sg.InputText(key='_WO_')],
             [sg.Text("Package Batch #:")],
             [sg.InputText(key='_PACKBATCH_')],
             [sg.Text("Master Batch #:")],
             [sg.InputText(key='_MASTERBATCH_')],
             [sg.Text("Environment:"), sg.Combo(['Prod', 'Test'], key='_ENVIRONMENT_', default_value='Prod')],
-            [sg.Button('Run'), sg.Button('Cancel')] ]
+            [sg.Button('Run'), sg.Button('Cancel')],
+            [sg.Text("Output:")], 
+            [sg.Output(size =(90, 10), key='_OUTPUT_')],
+            [sg.Button('Clear')]  ]
+
 
 # Create actual window to display
 window = sg.Window('Log Finder', layout, location=(500,275)) #note: location of window specific to this computer.
@@ -20,8 +40,9 @@ window = sg.Window('Log Finder', layout, location=(500,275)) #note: location of 
 # Event loop
 while True:
     event, values = window.read()
-    if event in (None, 'Cancel'):
+    if event in (None, 'Cancel', 'Exit'):
         break
+    
     if event == 'Run':
         if (values['_WO_'] == '' and values['_PACKBATCH_'] == '' and values['_MASTERBATCH_'] == ''):
             print('Nothing to find.')
@@ -32,5 +53,10 @@ while True:
                 print("No results found")
             else:
                 print(results)
+    
+    #elif event == 'Search Paths':
+    elif event == 'Clear':
+        window.FindElement('_OUTPUT_').Update('')
+
 
 window.close()
